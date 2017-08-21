9de4a1a199e0749e65dcbed2bb3235061ccfc9a7
Marcos Avila Isidoro <marcavis@yahoo.com.br>
2017-08-20 23:40:25 -0300

    representação mais simples de grafos não valorados: ok

--- grafoMain.py
+++ grafoMain.py
@@ -3,8 +3,11 @@
 #trabalho 1 - representação de grafos
 #Caio Silveira Batista, Marcos Avila Isidoro
 
+
+vertices = []
+listaDeArestas = []
+
 def main():
-    vertices = []
 
     seOrientado = ""
     while(seOrientado not in ["1", "2"]):
@@ -41,10 +44,48 @@ def main():
         except ValueError:
             qtVertices = 0
 
-    for i in range(qtVertices):
+    criarVertices(qtVertices)
+
+    print("*** Definição de arestas ***")
+    cancelou = False
+    while(cancelou == False):
+        print("A qualquer ponto, para encerrar a criação de arestas, digite \"sair\"")
+        strEntrada = ""
+        while(cancelou == False and strEntrada.upper() not in vertices):
+            print("Qual o vértice de entrada?")
+            print("Vértices permitidos: do V1 ao V" + str(qtVertices))
+            strEntrada = input()
+            if(strEntrada.upper() == "SAIR"):
+                cancelou = True
+        strSaida = ""
+        while(cancelou == False and strSaida.upper() not in vertices):
+            print("Qual o vértice de saída?")
+            print("Vértices permitidos: do V1 ao V" + str(qtVertices))
+            strSaida = input()
+            if(strSaida.upper() == "SAIR"):
+                cancelou = True
+        if(cancelou == False):
+            adicionarAresta(strEntrada, strSaida)
+
+    #mostrar as representações
+    mostraVertices()
+    mostraListaDeArestas()
+
+def criarVertices(quantidade: int):
+    for i in range(quantidade):
         vertices.append("V"+str(i+1))
 
-    for i in vertices: print(i)
+def adicionarAresta(entrada: str, saida: str):
+    nomeDaAresta = "E" + str(len(listaDeArestas) + 1)
+    listaDeArestas.append((nomeDaAresta, entrada.upper(), saida.upper()))
+
+def mostraVertices():
+    print("Vértices:", vertices)
+
+def mostraListaDeArestas():
+    print("Arestas:")
+    for aresta in listaDeArestas:
+        print(aresta)
 
 if __name__ == "__main__":
     main()
